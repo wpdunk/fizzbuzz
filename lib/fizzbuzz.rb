a96ab7fdffbc5e17d0c620c2b3c362117f4d3073
William Dunk <williamdunk@gmail.com>
2018-12-21 13:34:11 +0000

    broken trying to use alterntive example code for rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,21 +1,23 @@
-def fizzbuzz(number)
-  if number.is_a? Integer 
-    if number % 3 == 0 && number % 5 == 0
-      'fizzbuzz'
-    elsif number % 3 == 0
-      'fizz'
-    elsif number % 5 == 0
-      'buzz'
+class Check
+
+  def fizzbuzz(number)
+    if number.is_a? Integer
+      if number % 3 == 0 && number % 5 == 0
+        'fizzbuzz'
+      elsif number % 3 == 0
+        'fizz'
+      elsif number % 5 == 0
+        'buzz'
+      else
+        number
+      end
     else
-      number
+      "ERROR"
     end
-  else
-    "ERROR"
   end
+
 end
 
 # def fizzbuzz (num)
 #   num.is_a? Integer ? (num % 3 == 0 && num % 5 == 0 ? 'fizzbuzz' : (num % 3 == 0 ? 'fizz' : (num % 5 == 0 ? 'buzz' : num))) : 'ERROR'
 # end
-
- 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,29 @@
 require_relative '../lib/fizzbuzz'
 
-describe 'fizzbuzz' do
+describe Check do
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+  describe '.fizzbuzz' do
 
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
+    it 'returns "fizz" when passed 3' do
+      expect(3.fizzbuzz).to eq 'fizz'
+    end
 
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
+    it 'returns "buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
 
-  it 'returns 7 when passed 7' do
-    expect(fizzbuzz(7)).to eq 7
-  end
+    it 'returns "fizzbuzz" when passed 15' do
+      expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
 
-  it 'returns ERROR when passed "text"' do
-    expect(fizzbuzz("text")).to eq "ERROR"
+    it 'returns 7 when passed 7' do
+      expect(fizzbuzz(7)).to eq 7
+    end
+
+    it 'returns ERROR when passed "text"' do
+      expect(fizzbuzz("text")).to eq "ERROR"
+    end
+
   end
 
 end
