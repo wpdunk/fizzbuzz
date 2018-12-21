13e5e3b37b7a97af367f6c97906385ea89691028
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-21 15:06:17 +0000

    Added other tests to the Integer class extension

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,12 +24,20 @@ end
 describe Integer do
   describe '.fizzbuzz' do
 
-    # it 'returns "buzz" when passed 5' do
-    #   expect(Integer.fizzbuzz(5)).to eq 'buzz'
-    # end
-
     it 'returns "fizz" when passed 3' do
       expect(3.fizzbuzz).to eq 'fizz'
+    end
+
+    it 'returns "buzz" when passed 5' do
+      expect(5.fizzbuzz).to eq 'buzz'
+    end
+
+    it 'returns "fizzbuzz" when passed 15' do
+      expect(15.fizzbuzz).to eq 'fizzbuzz'
+    end
+
+    it 'returns 7 when passed 7' do
+      expect(7.fizzbuzz).to eq 7
     end
 
   end
