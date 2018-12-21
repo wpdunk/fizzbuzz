1cb1ec0e5b415450e16d16f725234bf2a6a0403a
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-21 12:01:50 +0000

    Added test for buzz, watched it fail

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
 end
+
