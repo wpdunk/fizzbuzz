fc39edcf5e6543ef449f742609e05c714ecfcbec
William Dunk <williamdunk@gmail.com>
2018-12-21 16:28:19 +0000

    just a little tidy up of the code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,26 +1,30 @@
 require_relative '../lib/fizzbuzz'
 
-describe '.fizzbuzz' do
+# describe '.fizzbuzz' do
+#
+#     it 'returns "fizz" when passed 3' do
+#       expect(fizzbuzz(3)).to eq 'fizz'
+#     end
+#
+#     it 'returns "buzz" when passed 5' do
+#       expect(fizzbuzz(5)).to eq 'buzz'
+#     end
+#
+#     it 'returns "fizzbuzz" when passed 15' do
+#       expect(fizzbuzz(15)).to eq 'fizzbuzz'
+#     end
+#
+#     it 'returns 7 when passed 7' do
+#       expect(fizzbuzz(7)).to eq 7
+#     end
+#
+#     it 'returns ERROR when passed "text"' do
+#       expect(fizzbuzz("text")).to eq "ERROR"
+#     end
+#
+# end
 
-    # it 'returns "buzz" when passed 5' do
-    #   expect(fizzbuzz(5)).to eq 'buzz'
-    # end
-
-    # it 'returns "fizzbuzz" when passed 15' do
-    #   expect(fizzbuzz(15)).to eq 'fizzbuzz'
-    # end
-    #
-    # it 'returns 7 when passed 7' do
-    #   expect(fizzbuzz(7)).to eq 7
-    # end
-    #
-    # it 'returns ERROR when passed "text"' do
-    #   expect(fizzbuzz("text")).to eq "ERROR"
-    # end
-
-end
-
-
+#alternate method
 describe Integer do
   describe '.fizzbuzz' do
 
